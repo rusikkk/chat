04333e1342548a90aa6cb64c7883d6bb17feb034
rusikkk <ruslan_box@centrum.cz>
2018-07-27 22:52:57 +0200

    create Server

--- src/main/java/com/rusya/chat/server/ChatServer.java
+++ src/main/java/com/rusya/chat/server/ChatServer.java
@@ -1,7 +1,39 @@
 package com.rusya.chat.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 /**
  * Created by rusJA
  */
 public class ChatServer {
+    public static void main(String[] args) throws InterruptedException {
+        new ChatServer(8000).run();
+
+    }
+
+    private final int port;
+
+    public ChatServer(int port) {
+        this.port = port;
+    }
+
+    public void run() throws InterruptedException{
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap bootstrap = new ServerBootstrap()
+                    .group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChatServerInitializer());
+            ChannelFuture future = bootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
 }
