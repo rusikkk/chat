021b64fcba11ac67117e46b3670821702271c557
rusikkk <ruslan_box@centrum.cz>
2018-07-28 02:37:36 +0200

    create Client

--- src/main/java/com/rusya/chat/client/ChatClientHandler.java
+++ src/main/java/com/rusya/chat/client/ChatClientHandler.java
@@ -1,7 +1,13 @@
 package com.rusya.chat.client;
 
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundMessageHandlerAdapter;
+
 /**
  * Created by rusJA
  */
-public class ChatClientHandler {
+public class ChatClientHandler extends ChannelInboundMessageHandlerAdapter<String> {
+    public void messageReceived(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+        System.out.println(s);
+    }
 }
--- src/main/java/com/rusya/chat/client/ChatClientInitializer.java
+++ src/main/java/com/rusya/chat/client/ChatClientInitializer.java
@@ -1,7 +1,24 @@
 package com.rusya.chat.client;
 
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+
 /**
  * Created by rusJA
  */
-public class ChatClientInitializer {
+public class ChatClientInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+        pipeline.addLast("decoder", new StringDecoder());
+        pipeline.addLast("encoder", new StringEncoder());
+        pipeline.addLast("handler", new ChatClientHandler());
+    }
 }
